12aae4ef6f20eebdbd426f04e13b394d1171dc43
horaciocoronel <arielc@gmail.com>
2017-11-15 15:07:00 -0500

    Add Ajax call to API, parse data and create li element for each candidate

--- election.js
+++ election.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  var url = 'https://bb-election-api.herokuapp.com/'
+  var candidatesList = document.querySelector('#candidates');
+  $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(data){
+      for (var i = 0; i < data.candidates.length; i++) {
+        var newLi = document.createElement('li');
+        newLi.innerHTML = "Name: " + data.candidates[i].name
+        + " | Votes: " + data.candidates[i].votes;
+        candidatesList.append(newLi)
+        console.log(data.candidates[i].name);
+      }
 
+      // console.log(data);
+    })
 });
